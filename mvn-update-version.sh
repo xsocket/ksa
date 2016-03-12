503e21f1e0c79e66bb6ac9fcb350d13cbc925d8e
麻文强 <mawenqiang1985@sina.com>
2016-03-12 14:40:21 +0800

    软件升级到3.8.6版本

--- mvn-update-version.sh
+++ mvn-update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '===============更新项目版本==============='
-mvn versions:set -DnewVersion=3.8.4
+mvn versions:set -DnewVersion=3.8.6
 
 echo '===============移除备份文件==============='
 find . -name "pom.xml.versionsBackup" -exec rm -r {} \;
