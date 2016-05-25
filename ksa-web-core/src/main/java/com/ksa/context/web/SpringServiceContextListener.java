a92792874f573c56ba9f3db2b2ab2ebc689f3f1b
麻文强 <xsocket@163.com>
2016-05-25 08:20:40 +0800

    切换连接配置，将连接池改为druid

--- ksa-web-core/src/main/java/com/ksa/context/web/SpringServiceContextListener.java
+++ ksa-web-core/src/main/java/com/ksa/context/web/SpringServiceContextListener.java
@@ -1,14 +1,8 @@
 package com.ksa.context.web;
 
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.ContextLoader;
 
@@ -23,8 +17,6 @@ import com.ksa.context.spring.SpringServiceContext;
  * @since v0.0.1
  */
 public class SpringServiceContextListener extends ContextLoader implements ServletContextListener {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger( SpringServiceContextListener.class );
     
     /*
      * (non-Javadoc)
@@ -36,22 +28,7 @@ public class SpringServiceContextListener extends ContextLoader implements Servl
         ApplicationContext context = this.initWebApplicationContext( event.getServletContext() );
         ServiceContextUtils.init( new SpringServiceContext( context ) );
         
-        RuntimeConfiguration.init( event.getServletContext() );
-        
-        checkDatabase( context );
-    }
-    
-    private void checkDatabase( ApplicationContext context ) {
-        BasicDataSource ds = context.getBean( BasicDataSource.class );
-        try { 
-            DatabaseMetaData data = ds.getConnection().getMetaData();
-            StringBuilder sb = new StringBuilder();
-            sb.append( "数据库连接成功：" ).append( data.getDatabaseProductName() ).append( "-" ).append( data.getDatabaseProductVersion() );
-            sb.append( "；用户名：" ).append( data.getUserName() );
-            LOGGER.info( sb.toString() );
-        } catch( SQLException e ) {
-            LOGGER.warn( "数据库连接失败！", e );
-        }
+        RuntimeConfiguration.init( event.getServletContext() );        
     }
 
     /*
