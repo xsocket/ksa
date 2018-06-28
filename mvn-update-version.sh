3e6ca5e1a218877a830b403f4950ae18cebe0909
麻文强 <xsocket@163.com>
2018-06-28 15:27:56 +0800

    升级到版本3.9.2

--- mvn-update-version.sh
+++ mvn-update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '===============更新项目版本==============='
-mvn versions:set -DnewVersion=3.9.1
+mvn versions:set -DnewVersion=3.9.2
 
 echo '===============移除备份文件==============='
 find . -name "pom.xml.versionsBackup" -exec rm -r {} \;
